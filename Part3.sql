ac721958a1514a9d2a8b1348e68a56b85099e96d
Vaiibhavi_Singh <vaiibhavisingh855@gmail.com>
2026-07-07 20:39:36 +0530

    Add files via upload

---
+++ Part3.sql
@@ -0,0 +1,5 @@
+Select * from products order by unit_price asc;
+select * from customers order by company_name;
+select * from employees order by hire_date desc;
+select * from orders order by order_date;
+select * from products order by units_in_stock desc;
